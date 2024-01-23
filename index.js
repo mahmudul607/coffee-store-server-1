ef62556e8d1e5473a310fe5879f256018157ed8c
unknown <mahmudul.cse.eng@gmail.com>
2024-01-24 00:06:05 +0600

    All most finish this project

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ require('dotenv').config()
 
 // middleware
 
-app.use(cors());
+
+
+
 app.use(express.json());
+app.use(
+  cors({
+    origin: ['https://coffee-store-one.web.app'],
+    credentials: true
+  })
+  );
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c4v4qwp.mongodb.net/?retryWrites=true&w=majority`;
 
